2e8ed0c4f0c7b08eef3df77447ce6e44c0fa6243
Miketheanalyst <126205859+Miketheanalyst@users.noreply.github.com>
2024-04-29 23:15:26 -0600

    Initial commit

---
+++ Data Engineering/Table Creation.sql
@@ -0,0 +1,50 @@
+-- Create 'departments' table
+CREATE TABLE departments (
+  dept_no VARCHAR(50) PRIMARY KEY,
+  dept_name VARCHAR(50) NOT NULL
+ );
+
+
+-- Create 'titles' table
+CREATE TABLE titles (
+  title_id VARCHAR(50) PRIMARY KEY,
+  title VARCHAR(50) NOT NULL
+);
+
+-- Create 'employees' table
+CREATE TABLE employees (
+  emp_no INT PRIMARY KEY,
+  title_id VARCHAR(50) REFERENCES titles(title_id),
+  birth_date DATE NOT NULL,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  sex VARCHAR(50) NOT NULL,
+  hire_date DATE NOT NULL
+);
+
+-- Create 'salaries' table, assuming emp_no is the foreign key referencing employees
+CREATE TABLE salaries (
+  emp_no INT REFERENCES employees(emp_no),
+  salary INT NOT NULL,
+  PRIMARY KEY (emp_no, salary)
+);
+
+-- Create 'dept_emp' table, this assumes a composite key because an employee can belong to more than one department over time
+CREATE TABLE dept_emp (
+  emp_no INT REFERENCES employees(emp_no),
+  dept_no VARCHAR(50) REFERENCES departments(dept_no),
+  PRIMARY KEY (emp_no, dept_no)
+);
+
+-- Create 'dept_manager' table, assuming a composite key because a department can have more than one manager over time
+CREATE TABLE dept_manager (
+  dept_no VARCHAR(50) REFERENCES departments(dept_no),
+  emp_no INT REFERENCES employees(emp_no),
+  PRIMARY KEY (dept_no, emp_no)
+);
+
+
+
+
+
+
